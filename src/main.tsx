b652997104f082a86cb7d0c4951e90c946230dc8
MadMilliner <jasonapeterson@gmail.com>
2025-10-08 15:54:29 -0700

    Conditions now toggle with version button in upper right corner

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import BattleTracker from "./components/BattleTracker/BattleTracker";
 import "./index.css";
 import App from "./App";
 import { About } from "./About";
+import ToggleComponent from "./components/ToggleContext";
+import { GlobalProvider } from "./hooks/versionContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <GlobalProvider>
   <React.StrictMode>
-    <About />
+    <div id="header">
+      <About /><ToggleComponent />
+    </div>
     <App />
   </React.StrictMode>
+  </GlobalProvider>
 );
