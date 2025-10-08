7876cb69a9951b3d7cc5f0c966a375c90f47fb6c
MadMilliner <jasonapeterson@gmail.com>
2025-10-08 09:43:05 -0700

    Added About page with instructions and credits

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import BattleTracker from "./components/BattleTracker/BattleTracker";
 import "./index.css";
 import App from "./App";
+import { About } from "./About";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <About />
     <App />
   </React.StrictMode>
 );
